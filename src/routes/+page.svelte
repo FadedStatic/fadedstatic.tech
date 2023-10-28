b1e9bebbba2bfc0fc7542b93740b815937913a75
Verbum <static.faded.cpp@gmail.com>
2023-10-28 15:22:29 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Particles from 'svelte-particles';
+	import { loadFull } from 'tsparticles';
+	import ParticlesJson from '../particles.json';
+    import { loadPolygonPath } from 'tsparticles-path-polygon';
+
+	let particlesInit = async (engine) => {
+		await loadPolygonPath(engine);
+		await loadFull(engine);
+	};
+</script>
+
+<div>
+	<Particles
+		id="tsparticles"
+		style=""
+		options={ParticlesJson}
+		{particlesInit}
+	/>
+</div>
